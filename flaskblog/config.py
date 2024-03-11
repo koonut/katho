3ebbdd5f7f1bccdabe225b487ac86c7908c9b60e
koonut <111723431+koonut@users.noreply.github.com>
2024-03-11 14:24:49 +0530

    link to live service of aiven for mysql

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,9 +1,10 @@
 import os
 
 
+
 class Config:
     SECRET_KEY = 'password'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost/katho'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://avnadmin:AVNS_oemweulDq4kiR6uMeHD@mysql-2f1d03e1-mca-c271.a.aivencloud.com:21594/katho'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
